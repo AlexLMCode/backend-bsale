5394cc23de3187e0fc571bb884f527b3fe447f72
Alex Lujan <alexlujanm@outlook.com>
2022-09-27 10:18:48 -0500

    Added things to controller and service products

--- controllers/products.ts
+++ controllers/products.ts
@@ -4,10 +4,14 @@ import productService from '../services/products';
 
 const getProducts = async (request: Request, res: Response) => {
   try {
-    const { productName } = request.query;
-    const data = await productService.getProductFromRepository();
+    const { productName, categoryId } = request.query;
+    const data = await productService.getProductFromRepository(
+      productName as string,
+      +categoryId
+    );
     return res.json(new CustomResponse(200, 'Success', data, undefined));
   } catch (error) {
+    console.log(error);
     return res
       .status(500)
       .json(new CustomResponse(500, error.message, undefined, error));
--- services/products.ts
+++ services/products.ts
@@ -1,17 +1,19 @@
 import Product from '../persistence/models/product';
 import { Like } from 'typeorm';
 
-const getProductFromRepository = async (name?: string) => {
+const getProductFromRepository = async (name?: string, categoryId?: number) => {
+  console.log('namememme', name);
   try {
-    const query = name
-      ? {
-          where: {
-            name: Like(`%${name} #%`),
-          },
-        }
-      : {};
+    const query =
+      name || categoryId
+        ? {
+            name: name ? Like(`%${name}%`) : undefined,
+            category: categoryId ? categoryId : undefined,
+          }
+        : {};
+    console.log('q', query);
     const productRepository = global.dataSource.getRepository(Product);
-    const products = await productRepository.find(query);
+    const products = await productRepository.findBy(query);
     return products;
   } catch (error) {
     console.error('Error In Repository', error);
