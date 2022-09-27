2600513e6a26e3f1224c0b1513493ba3f96cac13
Alex Lujan <alexlujanm@outlook.com>
2022-09-27 10:37:00 -0500

    Added things to categories controller

--- controllers/categories.ts
+++ controllers/categories.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
 import CustomResponse from '../common/response';
+import categoriesService from '../services/categories';
 
 const getCategories = async (request: Request, response: Response) => {
   try {
+    const data = await categoriesService.getCategoriesFromRepository();
+    response.json(new CustomResponse(200, 'Success', data, undefined));
   } catch (error) {
     response
       .status(500)
